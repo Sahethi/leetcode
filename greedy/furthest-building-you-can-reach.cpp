654fecdddc03896f6e83e11b4a061165418214f3
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-21 02:10:40 -0500

    LC-furthest-building-you-can-reach [Runtime: 15 ms; Memory: 58.2 MB]

--- greedy/furthest-building-you-can-reach.cpp
+++ greedy/furthest-building-you-can-reach.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int i = 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
         for(i=0; i<heights.size()-1; i++){
             if(heights[i] < heights[i+1]){
-                if(bricks == 0 && ladders == 0){
-                    return i;
-                }
-                int diff = heights[i+1] - heights[i];
-                if(diff <= bricks){
-                    if(bricks != 0){
-                        bricks -= diff;
-                    }else{
-                        if(ladders != 0) ladders--;
-                        else break;
-                    }
-                }else{
-                    if(ladders != 0) {
-                        ladders--;
-                     } else return i;
+                int climb = heights[i+1] - heights[i];
+                pq.push(climb);
+                if(pq.size() > ladders){
+                    int top = pq.top();
+                    pq.pop();
+                    bricks -= top;
+                    if (bricks < 0) return i;
                 }
             }
         }
