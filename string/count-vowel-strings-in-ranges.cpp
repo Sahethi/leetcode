f8635f80e364690dad13ad81d250bc2114d1ad7e
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-21 00:39:45 -0500

    LC-count-vowel-strings-in-ranges [Runtime: N/A; Memory: N/A]

--- string/count-vowel-strings-in-ranges.cpp
+++ string/count-vowel-strings-in-ranges.cpp
@@ -1,4 +1,5 @@
 class Solution {
+//brute force
 private:
     bool isVowel(char c){
         if(c == 'a' || c == 'e' || c == 'i' || c =='o' || c=='u')
@@ -8,6 +9,7 @@ private:
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
         vector<int> result;
+        unordered_map<string, bool> voweledWords;
         for(auto& query : queries){
             int count = 0;
             int l = query[0];
@@ -15,7 +17,14 @@ public:
             for(int i = l; i<=r; i++){
                 char first = words[i][0];
                 char last  = words[i][words[i].size() - 1];
-                if(isVowel(first) && isVowel(last)) count++;
+                if(voweledWords[words[i]]){
+                    count++;
+                    continue;
+                }
+                else if(isVowel(first) && isVowel(last)){
+                    count++;
+                    voweledWords[words[i]] = true;
+                }
             }
             result.push_back(count);
         }
