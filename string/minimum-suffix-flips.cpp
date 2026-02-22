1087fdfb64cbcf5c0c8c447c5099edd23e2e6475
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-22 16:33:37 -0500

    LC-minimum-suffix-flips [Runtime: 0 ms; Memory: 12.3 MB]

--- string/minimum-suffix-flips.cpp
+++ string/minimum-suffix-flips.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int minFlips(string target) {
-        
-        int count = 0;
-        for(int i=1; i<target.size(); i++){
-            if(target[i] ^ target[i+1])
-                count++;
+    int count = 0;
+    char state = '0';
+    for (char c : target) {
+        if (c != state) {
+            count++;
+            state = c;
         }
-        return (target[0] == '0' ? 0 : count+1);
     }
+    return count;
+}
 };
