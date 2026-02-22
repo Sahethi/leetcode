c45d4eb774c11efed5e37acdc090853a81cef7e4
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-22 16:38:28 -0500

    LC-minimum-suffix-flips [Runtime: 3 ms; Memory: 12.5 MB]

--- greedy/minimum-suffix-flips.cpp
+++ greedy/minimum-suffix-flips.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    int minFlips(string target) {
-    int count = 0;
-    char state = '0';
-    for (char c : target) {
-        if (c != state) {
-            count++;
-            state = c;
+    // int minFlips(string target) {
+    //     int count = 0;
+    //     char state = '0';
+    //     for (char c : target) {
+    //         if (c != state) {
+    //             count++;
+    //             state = c;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    int minFlips(string target){
+        int count = 0;
+        for(int i=1; i<target.size(); i++){
+            if(target[i] ^ target[i-1]){
+                count++;
+            }
         }
+        return target[0] == '0' ? count : count+1;
     }
-    return count;
-}
 };
