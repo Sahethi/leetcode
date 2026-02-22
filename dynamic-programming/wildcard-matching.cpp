b6f405f361a0ab84e9eeda611d3cd6cace862d3b
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-22 16:23:25 -0500

    LC-wildcard-matching [Runtime: 114 ms; Memory: 55.1 MB]

--- dynamic-programming/wildcard-matching.cpp
+++ dynamic-programming/wildcard-matching.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     vector<vector<int>> memo;
     bool dfs(int i, int j, string& s, string& p){
-        if(j == p.size()){
+        if(j == p.size())
             return i == s.size();
-        }
 
-        if(memo[i][j] != -1){
+        if(memo[i][j] != -1)
             return memo[i][j];
-        }
         
         bool ans=false;
-        if(p[j] == '*'){
+        if(p[j] == '*')
             ans = dfs(i, j+1, s, p) || (i < s.size() && dfs(i+1, j, s, p));
-        }else{
-            bool firstMatch = (i < s.size() &&
-                          (s[i] == p[j] || p[j] == '?'));
-
+        else{
+            bool firstMatch = (i < s.size() && (s[i] == p[j] || p[j] == '?'));
             ans = firstMatch && dfs(i+1, j+1, s, p);
         }
         memo[i][j] = ans;
