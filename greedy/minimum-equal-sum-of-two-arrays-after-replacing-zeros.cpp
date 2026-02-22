2618a700338d2c2e107811354dec6da0bdbbbf48
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-22 17:17:21 -0500

    LC-minimum-equal-sum-of-two-arrays-after-replacing-zeros [Runtime: 170 ms; Memory: 145.4 MB]

--- greedy/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ greedy/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -5,9 +5,8 @@ public:
         int zeroNum1 = 0;
         long long minSumNum1 = 0;
         for(int num1: nums1){
-            if(num1 == 0){
+            if(num1 == 0)
                 zeroNum1++;
-            }
             minSumNum1 += num1;
         }
         minSumNum1 += zeroNum1;
@@ -15,18 +14,15 @@ public:
         int zeroNum2 = 0;
         long long minSumNum2 = 0;
         for(int num2: nums2){
-            if(num2 == 0){
+            if(num2 == 0)
                 zeroNum2++;
-            }
             minSumNum2 += num2;
         }
         minSumNum2 += zeroNum2;
 
         if(minSumNum1 == minSumNum2) return minSumNum1;
-        if((minSumNum1 < minSumNum2) && (zeroNum1 == 0))
-            return -1;
-        if((minSumNum2 < minSumNum1) && (zeroNum2 == 0))
-            return -1;
+        if((minSumNum1 < minSumNum2) && (zeroNum1 == 0)) return -1;
+        if((minSumNum2 < minSumNum1) && (zeroNum2 == 0)) return -1;
         
         return max(minSumNum1,minSumNum2);
     }
