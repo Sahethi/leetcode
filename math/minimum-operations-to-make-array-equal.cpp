33409e004412a01a18691d349f7cda1f70076e3d
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-22 22:36:20 -0500

    LC-minimum-operations-to-make-array-equal [Runtime: 0 ms; Memory: 8.1 MB]

--- math/minimum-operations-to-make-array-equal.cpp
+++ math/minimum-operations-to-make-array-equal.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minOperations(int n) {
-        vector<int> arr(n);
+        // vector<int> arr(n);
         // int sum = 0;
         // for(int i=0; i<n; i++){
         //     arr[i] = (2 * i) + 1;
