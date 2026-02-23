290ceeeac5c796600a6d9cc5244e81bba1841b1b
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-22 22:56:31 -0500

    LC-button-with-longest-push-time [Runtime: 0 ms; Memory: 26.6 MB]

--- array/button-with-longest-push-time.cpp
+++ array/button-with-longest-push-time.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int buttonWithLongestTime(vector<vector<int>>& events) {
-        int maxTime = 0, maxIdx =0;
-        int i;
-        for(i=0; i<events.size()-1; i++){
-            int diff = events[i+1][1] - events[i][1];
-            if(maxTime < diff) maxIdx = events[i][0];
+        int maxTime = events[0][1], maxIdx = events[0][0];
+        for(int i=1; i<events.size(); i++){
+            int diff = events[i][1] - events[i-1][1];
+            if(maxTime < diff || (diff == maxTime && events[i][0] < maxIdx)){
+                maxTime = diff;
+                maxIdx = events[i][0];
+            }
             maxTime = max(maxTime, diff);
         }
-        int diff = events[i+1][1] - events[i][1];
-            if(maxTime < diff) maxIdx = events[i][0];
-            maxTime = max(maxTime, diff);
         return maxIdx;
     }
 };
