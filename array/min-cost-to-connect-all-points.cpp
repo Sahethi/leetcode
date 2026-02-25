a88d0f855b605b62924d9bd8ae5a109fb4e72eb4
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-25 03:18:02 -0500

    LC-min-cost-to-connect-all-points [Runtime: 19 ms; Memory: 14.5 MB]

---
+++ array/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        vector<int> visited(n, false);
+        vector<int> minDist(n, INT_MAX);
+        int totalCost = 0;
+        minDist[0] = 0;
+
+        for(int i=0; i<n; i++){
+            int u = -1;
+            for(int j=0; j<n; j++){
+                if(!visited[j] && (u == -1 || minDist[j] < minDist[u])){
+                    u=j;
+                }
+            }
+
+            visited[u] = true;
+            totalCost += minDist[u];
+
+            for(int v=0; v<n; v++){
+                if(!visited[v]){
+                    int cost = 
+                    abs(points[u][0] - points[v][0]) + 
+                    abs(points[u][1] - points[v][1]);
+
+                    minDist[v] = min(minDist[v], cost);
+                }
+            }
+
+        }
+
+        return totalCost;
+    }
+};
