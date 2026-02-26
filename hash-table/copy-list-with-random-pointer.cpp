0bb56a809278f7eef28b4cdac7d82dea0d6f20a8
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-26 13:46:12 -0500

    LC-copy-list-with-random-pointer [Runtime: 7 ms; Memory: 15.1 MB]

---
+++ hash-table/copy-list-with-random-pointer.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> mp;
+        Node* curr = head;
+
+        while(curr != NULL){
+            Node* copy = new Node(curr->val);
+            mp[curr] = copy;
+            curr = curr->next;
+        }
+
+        curr = head;
+        while(curr != NULL){
+            if(curr->next != NULL)
+                mp[curr]->next = mp[curr->next];
+            else 
+                mp[curr]->next = NULL;
+
+            if(curr->random != NULL)
+                mp[curr]->random = mp[curr->random];
+            else
+                mp[curr]->random = NULL;
+
+            curr = curr->next;
+        }
+
+        return mp[head];
+    }
+};
