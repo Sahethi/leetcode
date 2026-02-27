85f60a17ba19998e95fac2a1c45666622f0e7518
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-27 17:53:43 -0500

    LC-edit-distance [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/edit-distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1));
+
+        //if word2 is empty
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i; // delete i chars
+        }
+
+        //if word1 is empty
+        for(int j=0; j<=m; j++){
+            dp[j][0] = j; // delete i chars
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + min(
+                        dp[i - 1][j], 
+                        min(dp[i][j - 1], dp[i - 1][j - 1])
+                    );
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
