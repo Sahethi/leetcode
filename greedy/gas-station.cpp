58c1d590fc359c7cf106b367b3fa46b151058ac0
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-28 17:27:54 -0500

    LC-gas-station [Runtime: 3 ms; Memory: 112.4 MB]

--- greedy/gas-station.cpp
+++ greedy/gas-station.cpp
@@ -4,16 +4,17 @@ public:
         int tank = 0;
         int start = 0;
         int total = 0;
-        vector<int>diff(gas.size());
+
         for(int i=0; i<gas.size(); i++){
-            diff[i] = gas[i] - cost[i];
-            tank += diff[i];
-            total += diff[i];
+            int diff = gas[i] - cost[i];
+            tank += diff;
+            total += diff;
             if(tank < 0){
                 start = i+1;
                 tank = 0;
             }
         }
+
         if(total < 0) return -1;
         return start;
     }
