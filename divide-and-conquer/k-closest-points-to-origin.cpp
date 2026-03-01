b13ee95c24ea0d0a00da426e15aec1f72ecbb049
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-01 16:37:15 -0500

    LC-k-closest-points-to-origin [Runtime: 79 ms; Memory: 78.1 MB]

--- divide-and-conquer/k-closest-points-to-origin.cpp
+++ divide-and-conquer/k-closest-points-to-origin.cpp
@@ -6,7 +6,7 @@ public:
         for(auto& point : points){
             int x = point[0];
             int y = point[1];
-            int dist = sqrt(pow((0-x), 2) + pow((0-y),2));
+            int dist = pow((0-x),2) + pow((0-y),2);
             pq.push({dist, point});
             if(pq.size() > k){
                 pq.pop(); // keep only the k elements
