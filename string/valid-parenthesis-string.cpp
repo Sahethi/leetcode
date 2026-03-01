10770546fd761e771893ab2ad615a5a7836f5c22
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-01 00:47:55 -0500

    LC-valid-parenthesis-string [Runtime: 0 ms; Memory: 8.1 MB]

---
+++ string/valid-parenthesis-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> leftStack;
+        stack<int> starStack;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '*'){
+                starStack.push(i);
+            }else if(s[i] == '('){
+                leftStack.push(i);
+            }else{
+                if(!leftStack.empty()){
+                    leftStack.pop();
+                }else if(!starStack.empty()){
+                    starStack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        while(!leftStack.empty() &&!starStack.empty()){
+            int starTop = starStack.top();
+            int leftTop = leftStack.top();
+            if(starTop > leftTop){
+                leftStack.pop();
+                starStack.pop();
+            }else{
+                return false;
+            }
+        }
+
+        return leftStack.empty();
+    }
+};
