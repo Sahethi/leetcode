0e7a4f00af2799f234b055c74913925259264a78
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-02-28 23:21:04 -0500

    LC-partition-equal-subset-sum [Runtime: 87 ms; Memory: 13.4 MB]

--- dynamic-programming/partition-equal-subset-sum.cpp
+++ dynamic-programming/partition-equal-subset-sum.cpp
@@ -4,22 +4,20 @@ public:
         //Let dp[i][s] = can we form sum s using first i numbers?
         int n = nums.size();
         int total = 0;
-        int target = 0;
-        vector<bool> dp(target+1, false);
-        dp[0] = true;
-
-        for(int i=0; i<n; i++)
+        for(int i = 0; i < n; i++)
             total += nums[i];
 
-        if(total % 2 == 0){
-            target = total/2;
-            for(int i=0; i<n; i++){
-                for(int s=target; s>=nums[i]; s--){
-                    dp[s] = dp[s] || dp[s - nums[i]];
-                }
-            }
-        }else{
+        if(total % 2 != 0)
             return false;
+
+        int target = total / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for(int i=0; i<n; i++){
+            for(int s=target; s>=nums[i]; s--){
+                dp[s] = dp[s] || dp[s - nums[i]];
+            }
         }
         
         return dp[target];
