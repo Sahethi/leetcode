997bf9a1da81af76f2196b4de5ca6408fb8c77ff
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-01 17:03:24 -0500

    LC-k-closest-points-to-origin [Runtime: N/A; Memory: N/A]

--- array/k-closest-points-to-origin.cpp
+++ array/k-closest-points-to-origin.cpp
@@ -1,7 +1,45 @@
 class Solution {
+private: 
+    int dist(vector<int>& point){
+        int x = point[0];
+        int y = point[1];
+        return (x*x) + (y*y);
+    }
+
+    int partition(vector<vector<int>>& points, int left, int right){
+        int pivotDist = dist(points[right]);
+        int i = left;
+
+        for(int j=left; j<right; j++){
+            if(dist(points[j]) <= pivotDist){
+                swap(points[j], points[i]);
+            }
+        }
+        swap(points[i], points[right]);
+        return i;
+    }
+
+    void quickSelect(vector<vector<int>>& points, int left, int right, int k){
+        if(left >= right) return;
+        int pivot = partition(points, left, right);
+        if(pivot == k){
+            return;
+        }else if(pivot > k){
+            //search left
+            quickSelect(points, left, pivot-1, k);
+        }else{
+            //search right
+            quickSelect(points, pivot+1, right, k);
+        }
+    }
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        //u can use quick select cause u need the kth elements
+        //u can use quick select cause u need the kth elements only
+        quickSelect(points, 0, points.size()-1, k);
+        return vector<vector<int>>(points.begin(), points.begin()+k);
+
+        /*
+        //max-heap solution
         priority_queue<pair<int, vector<int>>> pq;
         for(auto& point : points){
             int x = point[0];
@@ -19,5 +57,6 @@ public:
             pq.pop();
         }
         return result;
+        */
     }
 };
