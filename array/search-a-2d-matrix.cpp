40fe358b1b429fb697573121bd1fb03085409c30
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-01 04:09:05 -0500

    LC-search-a-2d-matrix [Runtime: 0 ms; Memory: 13.3 MB]

---
+++ array/search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0;
+        int high = (m*n)-1;
+        int mid = 0;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            int val = matrix[row][col];
+
+            if(val < target){
+                low = mid + 1;
+            }else if(val > target){
+                high = mid - 1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
