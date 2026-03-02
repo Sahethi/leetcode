2960eaabe0f904f3748dd2b8d5d489808454c352
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-02 00:06:21 -0500

    LC-max-area-of-island [Runtime: 0 ms; Memory: 27.4 MB]

---
+++ union-find/max-area-of-island.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    int dfs(vector<vector<int>>& grid, int r, int c){
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if(r < 0 || c < 0 || r>=m || c>=n || grid[r][c] == 0) return 0;
+
+        grid[r][c] = 0;
+
+        return 1 + 
+            dfs(grid, r+1, c) +
+            dfs(grid, r-1, c) +
+            dfs(grid, r, c+1) +
+            dfs(grid, r, c-1);
+    }
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxArea = 0;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    int area = dfs(grid, i, j);
+                    maxArea = max(maxArea, area);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
