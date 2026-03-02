d236008280d7e42a65adfde663f4c67632a319db
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-02 00:31:26 -0500

    LC-best-time-to-buy-and-sell-stock [Runtime: 0 ms; Memory: 97.5 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock.cpp
@@ -5,15 +5,10 @@ public:
         int profit = 0;
 
         for(int i=0; i<prices.size(); i++){
-
-            //find the the day when the price is lowest, u buy on that day
-            if(prices[i] < buy){
+            if(prices[i] < buy)
                 buy = prices[i];
-            } else if (prices[i] - buy > profit) {
+            else if(prices[i] - buy > profit)
                 profit = prices[i] - buy;
-            }
-            //here what price we bought we will find diff with each element
-            //in our prices vector, store the max profit, if we didint find anythign it will be 0 by default
         }
 
         return profit;
