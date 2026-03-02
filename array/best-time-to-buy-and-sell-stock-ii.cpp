48b78c90a368e1d44bf2dcdfbdb2e9d9b0e7a3cd
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-02 03:03:47 -0500

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 0 ms; Memory: 19.9 MB]

--- array/best-time-to-buy-and-sell-stock-ii.cpp
+++ array/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        
         int hold = -prices[0];
         int notHold = 0;
+
         for(int i=1; i<prices.size(); i++){
             int prevHold = hold;
             int prevNotHold = notHold;
