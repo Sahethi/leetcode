87c409c4cf6cf2579af065b8148250de792ea4af
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-02 03:01:02 -0500

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 0 ms; Memory: 19.9 MB]

---
+++ dynamic-programming/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int hold = -prices[0];
+        int notHold = 0;
+        for(int i=1; i<prices.size(); i++){
+            int prevHold = hold;
+            int prevNotHold = notHold;
+
+            hold = max(prevHold, prevNotHold - prices[i]);
+            notHold = max(prevNotHold, prevHold + prices[i]);
+        }
+
+        return notHold;
+    }
+};
