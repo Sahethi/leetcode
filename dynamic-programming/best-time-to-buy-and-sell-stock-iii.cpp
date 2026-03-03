c1db94fed6bf9cd8d292b547236d1bc985996a2e
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-02 19:05:14 -0500

    LC-best-time-to-buy-and-sell-stock-iii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/best-time-to-buy-and-sell-stock-iii.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int buy1 = INT_MIN;
-        int buy2 = INT_MIN;
-        int sell1 = 0, sell2 = 0;
-
+        
         for(int price : prices){
+            int buy1 = INT_MIN, buy2 = INT_MIN, sell1 = 0, sell2 = 0;
+
             buy1 = max(buy1, -price);
             sell1 = max(sell1, buy1 + price);
             buy2 = max(buy2, sell1 - price);
