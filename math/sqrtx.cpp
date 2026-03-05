0a93fe84ef2aed8c8889663b4c66193b7067c125
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-05 01:19:00 -0500

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ math/sqrtx.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        //we have to do binary search
+        int left = 0;
+        int right = x;
+        
+        while(left <= right){
+            int mid = (left + right) >> 1; // int division by 2
+
+            if(mid * mid == x) return mid;
+            if(mid * mid < x){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        return right;
+        
+
+    }
+};
