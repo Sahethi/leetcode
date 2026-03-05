63c8214e13e29e451ba70a2a6ec1267d13418919
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-05 16:36:46 -0500

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

--- array/assign-cookies.cpp
+++ array/assign-cookies.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int contentKids = 0;
-        int j;
         
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
         int childPtr = 0, cookiePtr=0;
-        for(int i=0; i<g.size(); i++){
-            for(j=0; j<s.size(); j++){
-                if(cookiePtr < s.size() && childPtr < g.size() && s[cookiePtr] >= g[childPtr]){
+
+        while(cookiePtr < s.size() && childPtr < g.size()){
+            if(s[cookiePtr] >= g[childPtr]){
                     contentKids++;
                     childPtr++;
                     cookiePtr++;
-                    break;
                 }else{
                     childPtr++;
                 }
-            }
         }
 
         return contentKids;
