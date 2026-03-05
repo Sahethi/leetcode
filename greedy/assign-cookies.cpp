7fa1d162115f771fa7bc33ebef041db546d07434
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-05 16:25:01 -0500

    LC-assign-cookies [Runtime: N/A; Memory: N/A]

--- greedy/assign-cookies.cpp
+++ greedy/assign-cookies.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int contentKids = 0;
+        int j;
         for(int i=0; i<g.size(); i++){
-            for(int j=0; j<s.size(); j++){
+            for(j=0; j<s.size(); j++){
                 if(s[j] >= g[i]){
                     contentKids++;
                     break;
                 }
             }
+            if(j == s.size() && contentKids!=0) break;
         }
 
         return contentKids;
