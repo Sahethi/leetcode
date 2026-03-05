68a069f758afac922464f7849f78727b95121eca
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-05 01:20:03 -0500

    LC-sqrtx [Runtime: 2 ms; Memory: 8.6 MB]

--- binary-search/sqrtx.cpp
+++ binary-search/sqrtx.cpp
@@ -8,12 +8,9 @@ public:
         while(left <= right){
             long mid = (left + right) >> 1; // int division by 2
 
-            if(mid * mid == x) return mid;
-            if(mid * mid < x){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
+            if (mid * mid == x) return mid;
+            if (mid * mid < x) left = mid + 1;
+            else right = mid - 1;
         }
 
         return right;
