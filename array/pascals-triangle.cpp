e62fc44b8edf070c441c4ad131cbe465e70bae47
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-05 17:15:59 -0500

    LC-pascals-triangle [Runtime: 0 ms; Memory: 9.9 MB]

---
+++ array/pascals-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        if(numRows == 1) result.push_back({1});
+        if(numRows >= 2) {
+            result.push_back({1});
+            result.push_back({1,1});
+        }
+
+        for(int i=1; i<numRows-1; i++){
+            vector<int> ans;
+            ans.push_back(1);
+            int sum =0;
+            for(int j=0; j<result[i].size()-1; j++){
+                sum = result[i][j] + result[i][j+1];
+                ans.push_back(sum);
+            }
+            
+            ans.push_back(1);
+            result.push_back(ans);
+        }
+        return result;
+    }
+};
