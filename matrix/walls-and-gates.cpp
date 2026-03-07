277b74be4a84422942baa40ed2926455e7ab28cb
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-06 19:49:43 -0500

    LC-walls-and-gates [Runtime: 3 ms; Memory: 19 MB]

---
+++ matrix/walls-and-gates.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        queue<pair<int, int>> q;
+        for(int i=0; i<rooms.size(); i++){
+            for(int j=0; j<rooms[0].size(); j++){
+                if(rooms[i][j] == 0){
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while(!q.empty()){
+            auto gate = q.front();
+            q.pop();
+            int nr = gate.first;
+            int nc = gate.second;
+
+            if(nc + 1 < rooms[0].size()){
+                if(rooms[nr][nc+1] == INT_MAX){
+                    rooms[nr][nc+1] = rooms[nr][nc] + 1;
+                    q.push({nr,nc+1});
+                }
+            }
+
+            if(nr + 1 < rooms.size()){
+                if(rooms[nr+1][nc] == INT_MAX){
+                    rooms[nr+1][nc] = rooms[nr][nc] + 1;
+                    q.push({nr+1,nc});
+                }
+            }
+
+            if(nc - 1 >=0){
+                if(rooms[nr][nc-1] == INT_MAX){
+                    rooms[nr][nc-1] = rooms[nr][nc] + 1;
+                    q.push({nr,nc-1});
+                }
+            }
+
+            if(nr - 1 >=0){
+                if(rooms[nr-1][nc] == INT_MAX){
+                    rooms[nr-1][nc] = rooms[nr][nc] + 1;
+                    q.push({nr-1,nc});
+                }
+            }
+            
+        }
+    }
+};
