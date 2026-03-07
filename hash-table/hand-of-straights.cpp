29c46b75208361339f34cd39757a5d995946e113
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-07 03:48:09 -0500

    LC-hand-of-straights [Runtime: 32 ms; Memory: 31 MB]

---
+++ hash-table/hand-of-straights.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size() % groupSize != 0)
+            return false;
+        
+        sort(hand.begin(), hand.end());
+        //[1, 2, 3, 6, 2, 3, 4, 7, 8]
+        //{1, 2, 2, 3, 3, 4, 6, 7, 8}
+        int k = 0;
+        int total=0;
+        int index;
+        unordered_map<int, int> freq;
+        for(int i=0; i<hand.size(); i++){
+            int v = hand[i];
+            freq[v]++;
+        }
+
+        for(int x : hand){
+            if(freq[x] == 0) continue;
+            for(int i=0; i<groupSize; i++){
+                int card = x + i;
+                if(freq[card] == 0)
+                    return false;
+                freq[card]--;
+            }
+        }
+
+        return true;
+    }
+};
