f14dd1f77c25a098cc84c72bfc125919838a7460
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-12 22:34:51 -0400

    LC-implement-stack-using-queues [Runtime: N/A; Memory: N/A]

---
+++ queue/implement-stack-using-queues.cpp
@@ -0,0 +1,47 @@
+class MyStack {
+public:
+    queue<int> queue1;
+    queue<int> queue2;
+    int topEl;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+      queue1.push(x);  
+      topEl = x;
+    }
+    
+    int pop() {
+        while(!queue1.empty()){
+            if(queue1.size() == 1){
+                topEl = queue1.front();
+                queue1.pop();
+                swap(queue1, queue2);
+                break;
+            }
+            int front = queue1.front();
+            queue1.pop();
+            queue2.push(front);     
+        }
+        return topEl;
+    }
+    
+    int top() {
+        return topEl;
+    }
+    
+    bool empty() {
+        if(queue1.empty() && queue2.empty()) return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
