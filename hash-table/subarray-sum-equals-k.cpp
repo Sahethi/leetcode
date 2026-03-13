8139874c87881cd5de965bb4d285130a86f0acec
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-12 23:14:29 -0400

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        int prefixSum = 0;
+        unordered_map<int, int> count;
+        count[0] = 1;
+        int ans=0;
+        for(int i=0; i<nums.size(); i++){
+            prefixSum += nums[i];
+            count[prefixSum]++;
+            if(count[prefixSum - k] > 0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
