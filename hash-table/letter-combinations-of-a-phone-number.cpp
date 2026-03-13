d54bc62eb73f9f155ec71dce0a537c81114ff38d
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-13 02:31:20 -0400

    LC-letter-combinations-of-a-phone-number [Runtime: 0 ms; Memory: 9.5 MB]

--- hash-table/letter-combinations-of-a-phone-number.cpp
+++ hash-table/letter-combinations-of-a-phone-number.cpp
@@ -2,25 +2,74 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {};
-
-        unordered_map<char, string> mp = {
+        vector<string> res = {""};
+        unordered_map<char, string> map = {
             {'2', "abc"}, {'3', "def"},
             {'4', "ghi"}, {'5', "jkl"},
             {'6', "mno"}, {'7', "pqrs"},
-            {'8', "tuv"}, {'9', "wxyz"}
+            {'8', "tuv"}, {'9', "wxyz"},
         };
-
-        vector<string> result = {""};
-
+        string ans;
         for(char digit : digits){
             vector<string> temp;
-            string letters = mp[digit];
-            for(string s : result)
-                for(char letter : letters)
-                    temp.push_back(s + letter);
-            result = temp;
+            string letters = map[digit];
+            for(string s : res){
+                for(char letter : letters){
+                    temp.push_back(s+letter);
+                }
+            }
+            res = temp;
         }
+        
+        return res;
 
-        return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if(digits.empty()) return {};
+
+        // unordered_map<char, string> mp = {
+        //     {'2', "abc"}, {'3', "def"},
+        //     {'4', "ghi"}, {'5', "jkl"},
+        //     {'6', "mno"}, {'7', "pqrs"},
+        //     {'8', "tuv"}, {'9', "wxyz"}
+        // };
+
+        // vector<string> result = {""};
+
+        // for(char digit : digits){
+        //     vector<string> temp;
+        //     string letters = mp[digit];
+        //     for(string s : result)
+        //         for(char letter : letters)
+        //             temp.push_back(s + letter);
+        //     result = temp;
+        // }
+
+        // return result;
     }
 };
