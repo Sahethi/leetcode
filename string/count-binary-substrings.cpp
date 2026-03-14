0e3832dc015347957f85366750aa27ae1269111a
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-14 17:22:35 -0400

    LC-count-binary-substrings [Runtime: 7 ms; Memory: 12.9 MB]

--- string/count-binary-substrings.cpp
+++ string/count-binary-substrings.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int countBinarySubstrings(string s) {
         int ans=0;
-        vector<int> groups;
-        int prevGroup;
-        int currGroup;
-        for(int i=1; i<s.length()-1; i++){
+        // vector<int> groups;
+        int prevGroup=0;
+        int currGroup=1;
+
+        for(int i=1; i<s.length(); i++){
             if(s[i] == s[i-1]){
                 currGroup++;
             }else{
+                ans += min(prevGroup, currGroup);
                 prevGroup = currGroup;
-                groups.push_back(prevGroup);
                 currGroup = 1;
             }
         }
 
+        ans += min(prevGroup, currGroup);
+        
         return ans;
     }
 };
