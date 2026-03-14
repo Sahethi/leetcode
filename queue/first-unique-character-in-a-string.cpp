5d42ebebaabd605f8269e8cffc653f880c7bb2b1
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-13 23:28:29 -0400

    LC-first-unique-character-in-a-string [Runtime: 4 ms; Memory: 14.7 MB]

--- queue/first-unique-character-in-a-string.cpp
+++ queue/first-unique-character-in-a-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> count;
+        vector<int> freq(26, 0);
         for(char c : s){
-            count[c]++;
+            freq[c-'a']++;
         }
 
         for(int i=0; i<s.size(); i++){
-            if(count[s[i]] == 1){
+            if(freq[s[i]-'a'] == 1){
                 return i;
             }
         }
