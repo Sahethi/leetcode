32066d5bbcadfa0a7566e8d8b2a75c8c89aac7d1
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-18 17:05:11 -0400

    LC-longest-increasing-path-in-a-matrix [Runtime: 33 ms; Memory: 30 MB]

---
+++ breadth-first-search/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp, int n, int m){
+        if(dp[i][j] != -1) return dp[i][j];
+        vector<pair<int, int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        int ans = 1;
+        for(auto& [dx, dy] : dirs){
+            int ni = dx + i;
+            int nj = dy + j;
+
+            if(ni >= 0 && nj >= 0 && ni < m && nj < n &&
+                matrix[ni][nj] > matrix[i][j]){
+
+                ans = max(ans, 1+ dfs(ni, nj, matrix, dp, n, m));
+            }
+        }
+
+        return dp[i][j] = ans;
+
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int res = 0;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                res = max(res, dfs(i, j, matrix, dp, n, m));
+            }
+        }
+
+        return res;
+    }
+};
