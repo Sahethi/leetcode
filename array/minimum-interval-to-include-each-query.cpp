280da6cd65b38e96a9dfed2b69d00de95542482e
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-19 19:59:36 -0400

    LC-minimum-interval-to-include-each-query [Runtime: 67 ms; Memory: 117.3 MB]

--- array/minimum-interval-to-include-each-query.cpp
+++ array/minimum-interval-to-include-each-query.cpp
@@ -2,43 +2,44 @@ class Solution {
 public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
         //brute force will be TLE n*m
-        vector<pair<int, int>> queryIndex;
         int n = intervals.size();
         int m = queries.size();
 
-        vector<int> ans(m, -1);
+        vector<pair<int, int>> queriesIdx;
 
         int i=0;
-        for(int q : queries){
-            queryIndex.push_back({q, i++});
-        }
+        for(int q : queries)
+            queriesIdx.push_back({q, i++});
 
-        sort(queryIndex.begin(), queryIndex.end());
+        sort(queriesIdx.begin(), queriesIdx.end());
         sort(intervals.begin(), intervals.end());
 
-        //store size and right
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        //declare heap stores size and right
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-        int j = 0;
-        for(auto& [q, idx] : queryIndex){
+        int j=0;
+        vector<int> ans(m, -1);
+
+        for(auto& [q, idx] : queriesIdx){
+            
             while(j < n && intervals[j][0] <= q){
                 int l = intervals[j][0];
                 int r = intervals[j][1];
                 int size = r - l + 1;
-                
+
                 pq.push({size, r});
                 j++;
             }
+            
 
-            //if the right is less than q then pop
+            //pop any invalid intervals
+            //if right is < than q then pop
             while(!pq.empty() && pq.top().second < q){
                 pq.pop();
             }
 
             if(!pq.empty()){
                 ans[idx] = pq.top().first;
-            }else{
-                ans[idx] = -1;
             }
         }
 
