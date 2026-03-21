1abeffd8f4c726060da7d8980492362dca8e7c0c
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-21 03:45:22 -0400

    LC-sort-an-array [Runtime: N/A; Memory: N/A]

---
+++ counting-sort/sort-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int partition(vector<int>& nums, int left, int right){
+        int pivot = nums[right];
+        int i = left;
+
+        for(int j=left; j<right; j++){
+            if(nums[j] < pivot){
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+        swap(nums[i], nums[right]);
+        return i;
+    }
+
+    void quickSort(vector<int>& nums, int left, int right){
+        if(left >= right) return;
+
+        int pivotIndex = partition(nums, left, right);
+
+        quickSort(nums, left, pivotIndex - 1);
+        quickSort(nums, pivotIndex + 1, right);
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
