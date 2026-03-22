bb5ea5b6575dd6aaee5e5e5bb4d9c18a6a1880b9
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-22 19:01:40 -0400

    LC-permutations-ii [Runtime: 0 ms; Memory: 11.4 MB]

--- backtracking/permutations-ii.cpp
+++ backtracking/permutations-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    void backtrack(set<vector<int>>& result, vector<int>& curr,
+    void backtrack(vector<vector<int>>& result, vector<int>& curr,
     vector<bool>& used, vector<int>& nums){
 
         //made 1 permutation
         if(curr.size() == nums.size()){
-            result.insert(curr);
+            result.push_back(curr);
             return;
         }
 
         for(int i=0; i<nums.size(); i++){
             if(used[i]) continue;
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
 
             used[i] = true;
             curr.push_back(nums[i]);
@@ -20,11 +21,43 @@ class Solution {
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
         vector<int> curr;
         vector<bool> used(nums.size(), false);
         backtrack(result, curr, used, nums);
 
-        return vector<vector<int>>(result.begin(), result.end());
+        return result;
     }
 };
+
+// class Solution {
+//     void backtrack(set<vector<int>>& result, vector<int>& curr,
+//     vector<bool>& used, vector<int>& nums){
+
+//         //made 1 permutation
+//         if(curr.size() == nums.size()){
+//             result.insert(curr);
+//             return;
+//         }
+
+//         for(int i=0; i<nums.size(); i++){
+//             if(used[i]) continue;
+
+//             used[i] = true;
+//             curr.push_back(nums[i]);
+//             backtrack(result, curr, used, nums);
+//             curr.pop_back();
+//             used[i] = false;
+//         }
+//     }
+// public:
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         set<vector<int>> result;
+//         vector<int> curr;
+//         vector<bool> used(nums.size(), false);
+//         backtrack(result, curr, used, nums);
+
+//         return vector<vector<int>>(result.begin(), result.end());
+//     }
+// };
