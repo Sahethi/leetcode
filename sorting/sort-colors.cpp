239b62d8b918bd884d03ba4465e713b4cf77704d
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-22 00:21:58 -0400

    LC-sort-colors [Runtime: 0 ms; Memory: 11.7 MB]

--- sorting/sort-colors.cpp
+++ sorting/sort-colors.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+
+        int low = 0, mid = 0, high=nums.size()-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low++], nums[mid++]);
+            } else if (nums[mid] == 1){
+                mid++;
+            } else {
+                swap(nums[mid], nums[high--]);
+            }
+        }
+
+        /**
         const int OFFSET = 50000;
         const int SIZE = 100001;
 
@@ -10,13 +24,14 @@ public:
             count[n + OFFSET]++;
         }
 
-        int idx;
+        int idx=0;
         for(int i=0; i<SIZE; i++){
             while(count[i] > 0){
                 nums[idx++] = i - OFFSET;
                 count[i]--;
             }
         }
+        **/
 
     }
 };
