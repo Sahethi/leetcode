c3c71d3da575db834c96e48b2354b2ada793b849
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-22 19:18:31 -0400

    LC-boats-to-save-people [Runtime: N/A; Memory: N/A]

--- two-pointers/boats-to-save-people.cpp
+++ two-pointers/boats-to-save-people.cpp
@@ -3,7 +3,7 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
 
         int boats = 0;
-        sort(people.begin(), people.end());
+        // sort(people.begin(), people.end());
         bool last = false;
         for(int i=0; i<people.size()-1; i++){
             if(people[i] + people[i+1] <= limit) {
