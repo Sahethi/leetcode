0f3119a60f5a493b6085d350c794dabfc2d2d2b3
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-22 19:26:06 -0400

    LC-boats-to-save-people [Runtime: 17 ms; Memory: 45.9 MB]

--- greedy/boats-to-save-people.cpp
+++ greedy/boats-to-save-people.cpp
@@ -3,28 +3,20 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
 
         int boats = 0;
-        // sort(people.begin(), people.end());
-        bool last = false;
-        for(int i=0; i<people.size()-1; i++){
-            if(people[i] + people[i+1] <= limit) {
+        sort(people.begin(), people.end());
+        int l = 0, r = people.size()-1;
+        while(l <= r){
+            if(people[l] + people[r] <= limit){
                 boats++;
-                i++;
-                if(i == people.size()-1) last = true;
+                l++;
+                r--;
             }else{
-                if(people[i] <= limit){
-                    boats++;
-                }
-                last = false;
-            }
-        }
-
-        if(!last){
-            if(people[people.size()-1] <= limit){
+                r--;
                 boats++;
             }
         }
-
 
         return boats;
+        
     }
 };
