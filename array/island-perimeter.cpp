7fdb61f205d55f9a28ca09a1cfaeb642d4a9991a
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-22 04:05:35 -0400

    LC-island-perimeter [Runtime: 0 ms; Memory: 102.2 MB]

--- array/island-perimeter.cpp
+++ array/island-perimeter.cpp
@@ -3,30 +3,15 @@ public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> paddedGrid(m+2, vector<int>(n+2, 0));
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                paddedGrid[i][j] = grid[i-1][j-1];
-            }
-        }
 
         int peri = 0;
 
-        for(int i=1; i<=m; i++){
-            for(int j=1; j<=n; j++){
-                if(paddedGrid[i][j] == 1){
-                    if(paddedGrid[i-1][j] == 0){
-                        peri++;
-                    }
-                    if(paddedGrid[i+1][j] == 0){
-                        peri++;
-                    }
-                    if(paddedGrid[i][j-1] == 0){
-                        peri++;
-                    }
-                    if(paddedGrid[i][j+1] == 0){
-                        peri++;
-                    }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1){
+                    peri += 4;
+                    if(i>0 && grid[i-1][j] == 1) peri -= 2;
+                    if(j>0 && grid[i][j-1] == 1) peri -= 2;
                 }
             }
         }
