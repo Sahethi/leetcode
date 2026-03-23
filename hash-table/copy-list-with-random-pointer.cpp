823aae81ea7bf15702ca1f7f5ffbcefb81561db6
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-23 19:18:52 -0400

    LC-copy-list-with-random-pointer [Runtime: 7 ms; Memory: 15.1 MB]

--- hash-table/copy-list-with-random-pointer.cpp
+++ hash-table/copy-list-with-random-pointer.cpp
@@ -17,28 +17,75 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
-        //basically we are mapping which original node is mapped to the copy
-        Node* curr = head;
+        
         if(head == NULL) return NULL;
 
+        unordered_map<Node*, Node*> copyMap;
+        Node* curr = head;
         while(curr != NULL){
-            Node* copy = new Node(curr->val);
-            mp[curr] = copy;
+            copyMap[curr] = new Node (curr->val);
             curr = curr->next;
         }
-
+        
         curr = head;
         while(curr != NULL){
-            if(curr->next != NULL) mp[curr]->next = mp[curr->next];
-            else mp[curr]->next = NULL;
+            if(curr->next != NULL) copyMap[curr]->next = copyMap[curr->next];
+            else copyMap[curr]->next = NULL;
 
-            if(curr->random != NULL) mp[curr]->random = mp[curr->random];
-            else mp[curr]->random = NULL;
+            if(curr->random != NULL) copyMap[curr]->random = copyMap[curr->random];
+            else copyMap[curr]->random = NULL;
 
             curr = curr->next;
         }
+        return copyMap[head];
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // unordered_map<Node*, Node*> mp;
+        // //basically we are mapping which original node is mapped to the copy
+        // Node* curr = head;
+        // if(head == NULL) return NULL;
 
-        return mp[head];
+        // while(curr != NULL){
+        //     Node* copy = new Node(curr->val);
+        //     mp[curr] = copy;
+        //     curr = curr->next;
+        // }
+
+        // curr = head;
+        // while(curr != NULL){
+        //     if(curr->next != NULL) mp[curr]->next = mp[curr->next];
+        //     else mp[curr]->next = NULL;
+
+        //     if(curr->random != NULL) mp[curr]->random = mp[curr->random];
+        //     else mp[curr]->random = NULL;
+
+        //     curr = curr->next;
+        // }
+
+        // return mp[head];
     }
 };
