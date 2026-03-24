ca767cdeb4e95c92e7e029dce1574d6962b17b9b
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-23 22:11:08 -0400

    LC-construct-product-matrix [Runtime: N/A; Memory: N/A]

--- array/construct-product-matrix.cpp
+++ array/construct-product-matrix.cpp
@@ -3,19 +3,24 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        int mod = 12345;
+        vector<vector<int>> prodMat(m, vector<int>(n, 1));
 
-        vector<vector<int>> prodMat(m, vector<int>(n));
+        // result[i] = (product of left) * (product of right)
 
-        unsigned long long ans= 1;
+        int prefix = 1;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                ans *= grid[i][j];
+                prodMat[i][j] = prefix;
+                prefix = (prefix * grid[i][j]) % mod;
             }
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                prodMat[i][j] = (ans / grid[i][j]) % 12345;
+        int suffix = 1;
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                prodMat[i][j] = (prodMat[i][j] * suffix) % mod;
+                suffix = (suffix * grid[i][j]) % mod; 
             }
         }
 
