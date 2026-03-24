dadfddf5c74d390f5f4c770931f1b07abdb0b4f3
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-23 21:54:03 -0400

    LC-construct-product-matrix [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/construct-product-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> prodMat(m, vector<int>(n));
+
+        int ans= 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                ans *= grid[i][j];
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                prodMat[i][j] = (ans / grid[i][j]) % 12345;
+            }
+        }
+
+        return prodMat;
+    }
+};
