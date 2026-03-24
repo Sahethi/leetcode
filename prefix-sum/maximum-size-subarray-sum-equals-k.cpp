bfbf4e7405b88b5ba5b41423073a7244607628db
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-23 20:09:23 -0400

    LC-maximum-size-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/maximum-size-subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+
+        int maxLen = 0;
+        unordered_map<int, int> prefixIdx;
+        int prefixSum = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            prefixSum += nums[i];
+            if(prefixSum == k){
+                maxLen = i+1;
+            }
+            if(prefixIdx.find(prefixSum - k) != prefixIdx.end()){
+                maxLen = max(maxLen, i - prefixIdx[prefixSum - k]);
+            } else{
+                prefixIdx[prefixSum] = i;
+            }
+        }
+
+        return maxLen;
+    }
+};
