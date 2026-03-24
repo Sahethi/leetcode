fc9ad04a23e21efc00a6dac77a0a5cb296310d0c
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-23 22:19:23 -0400

    LC-construct-product-matrix [Runtime: 32 ms; Memory: 132 MB]

--- prefix-sum/construct-product-matrix.cpp
+++ prefix-sum/construct-product-matrix.cpp
@@ -11,6 +11,7 @@ public:
         long long prefix = 1;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
+                //just left
                 prodMat[i][j] = prefix;
                 prefix = (prefix * grid[i][j]) % mod;
             }
@@ -19,6 +20,7 @@ public:
         long long suffix = 1;
         for(int i=m-1; i>=0; i--){
             for(int j=n-1; j>=0; j--){
+                // left * right
                 prodMat[i][j] = (prodMat[i][j] * suffix) % mod;
                 suffix = (suffix * grid[i][j]) % mod; 
             }
