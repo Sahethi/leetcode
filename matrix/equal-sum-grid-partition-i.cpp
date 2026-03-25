6edec04a57324eafe9a3d3c5912b593dde87b430
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-24 23:04:09 -0400

    LC-equal-sum-grid-partition-i [Runtime: N/A; Memory: N/A]

--- matrix/equal-sum-grid-partition-i.cpp
+++ matrix/equal-sum-grid-partition-i.cpp
@@ -24,7 +24,7 @@ public:
         }
 
 
-        int leftSum = 0;
+        long long leftSum = 0;
         for(int j =0; j<n-1; j++){
             for(int i=0; i<m; i++){
                 leftSum += grid[i][j];
