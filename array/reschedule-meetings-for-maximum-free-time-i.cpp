4f3563a8afb1558075d6a2301e3b685998459000
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-25 02:58:56 -0400

    LC-reschedule-meetings-for-maximum-free-time-i [Runtime: 1 ms; Memory: 123.4 MB]

--- array/reschedule-meetings-for-maximum-free-time-i.cpp
+++ array/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        //you will first find all the gaps
         int n = startTime.size();
-        
-        // Build gaps array: n+1 gaps
-        vector<int> gaps(n + 1);
-        gaps[0] = startTime[0];                          // before first meeting
-        for (int i = 1; i < n; i++)
-            gaps[i] = startTime[i] - endTime[i - 1];    // between meetings
-        gaps[n] = eventTime - endTime[n - 1];            // after last meeting
+        vector<int>gaps(n+1);
+        gaps[0] = startTime[0];
+        for(int i=1; i<n; i++){
+            gaps[i] = startTime[i] - endTime[i-1];
+        }
+        gaps[n] = eventTime - endTime[n-1];
 
-        // Sliding window of size k+1
-        int windowSum = 0, ans = 0;
-        for (int i = 0; i <= n; i++) {
+        int windowSum = 0, maxGap = 0;
+
+        for(int i=0; i<=n; i++){
             windowSum += gaps[i];
-            if (i >= k) {                                // window is full
-                ans = max(ans, windowSum);
-                windowSum -= gaps[i - k];
+
+            if(i >= k){
+                maxGap = max(maxGap, windowSum);
+                windowSum -= gaps[i - k]; //remove the left dhrink
             }
         }
-        return ans;
+        return maxGap;
     }
 };
