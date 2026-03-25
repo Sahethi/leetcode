f7a9c80225169fdbb9779d9f8fa9e054f281d425
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-24 23:03:41 -0400

    LC-equal-sum-grid-partition-i [Runtime: N/A; Memory: N/A]

---
+++ array/equal-sum-grid-partition-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canPartitionGrid(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int sum = 0;
+        for(auto& row : grid){
+            for(int val : row){
+                sum += val;
+            }
+        }
+
+        int topSum = 0;
+        for(int i=0; i<m-1; i++){
+            for(int j=0; j<n; j++){
+                topSum += grid[i][j];
+            }
+
+            int bottomSum = sum - topSum;
+
+            if(bottomSum == topSum) return true;
+        }
+
+
+        int leftSum = 0;
+        for(int j =0; j<n-1; j++){
+            for(int i=0; i<m; i++){
+                leftSum += grid[i][j];
+            }
+
+            int rightSum = sum - leftSum;
+
+            if(rightSum == leftSum) return true;
+        }
+
+        return false;
+    }
+};
