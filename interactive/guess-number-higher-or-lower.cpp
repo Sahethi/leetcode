346e0bc3aa6204b6f5a5f9d4e5586b73a33d3e7a
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-30 00:51:12 -0400

    LC-guess-number-higher-or-lower [Runtime: N/A; Memory: N/A]

--- interactive/guess-number-higher-or-lower.cpp
+++ interactive/guess-number-higher-or-lower.cpp
@@ -17,13 +17,9 @@ public:
         while(l <= r){
             int mid =  l + (r-l)/2;
             int pick = guess(mid);
-            if(pick == -1){
-                r = mid - 1;
-            }else if(pick == 1){
-                l = mid + 1;
-            }else{
-                return mid;
-            }
+            if(pick == 0) return mid;
+            else if(pick == -1) r = mid + 1;
+            else if(pick == 1) l = mid - 1;
         }
         return 0;
     }
