49db660a095196039c0d0fcc0367ff7fc278a68e
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-30 00:52:08 -0400

    LC-guess-number-higher-or-lower [Runtime: 2 ms; Memory: 7.9 MB]

--- binary-search/guess-number-higher-or-lower.cpp
+++ binary-search/guess-number-higher-or-lower.cpp
@@ -18,8 +18,8 @@ public:
             int mid =  l + (r-l)/2;
             int pick = guess(mid);
             if(pick == 0) return mid;
-            else if(pick == -1) r = mid + 1;
-            else if(pick == 1) l = mid - 1;
+            else if(pick == -1) r = mid - 1;
+            else if(pick == 1) l = mid + 1;
         }
         return 0;
     }
