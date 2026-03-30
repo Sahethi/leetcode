ac81ac2822c3ce562626c9a6f25cd9fbc7271802
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-30 03:45:03 -0400

    LC-simplify-path [Runtime: 6 ms; Memory: 11.2 MB]

---
+++ string/simplify-path.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        /**
+        Input: "/.../a/../b/c/../d/./"
+        Output: "/.../b/d"
+        */
+
+        //think abotu splitting from /
+        // /home / /foo /
+        stringstream ss(path);
+        string token;
+        vector<string> st;
+
+        while(getline(ss, token, '/')){
+            if(token == "" || token == "."){
+                //do nothing
+            }else if(token == ".."){
+                if(!st.empty()) st.pop_back();
+            }else{
+                st.push_back(token);
+            }
+        }
+
+        string res = "/";
+        for(int i=0; i<st.size(); i++){
+            if(i == st.size()-1)
+                res += st[i];
+            else
+                res += st[i] + "/";
+        }
+        return res;
+
+    }
+};
