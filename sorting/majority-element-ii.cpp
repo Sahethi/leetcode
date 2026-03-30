3a19996d6129b21624cedc24a5086ee74202ea91
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-30 01:16:38 -0400

    LC-majority-element-ii [Runtime: 9 ms; Memory: 26.5 MB]

--- sorting/majority-element-ii.cpp
+++ sorting/majority-element-ii.cpp
@@ -3,23 +3,17 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         int times = floor(n/3);
-        vector<int> res;
-
-        unordered_set<int> unique_nums;
-
-        for(int n : nums){
-            unique_nums.insert(n);
-        }
+        unordered_set<int> res;
 
         unordered_map<int, int> count;
         for(int n : nums){
             count[n]++;
         }
 
-        for(int n : unique_nums){
-            if(count[n] > times) res.push_back(n);
+        for(int n : nums){
+            if(count[n] > times) res.insert(n);
         }
 
-        return res;
+        return vector<int>(res.begin(), res.end());
     }
 };
