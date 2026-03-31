4f5cb8e92c8a210e8d5b9f5c29d8c65aed56d8c7
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-31 00:07:50 -0400

    LC-minimum-size-subarray-sum [Runtime: 0 ms; Memory: 32.1 MB]

--- array/minimum-size-subarray-sum.cpp
+++ array/minimum-size-subarray-sum.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         
-        int sum = 0;
-        int left = 0;
-        int windowSize = INT_MAX;
+        int sum = 0, left = 0, windowSize = INT_MAX;
+        
         for(int right=0; right<nums.size(); right++){
             sum += nums[right];
             while(sum >= target){
