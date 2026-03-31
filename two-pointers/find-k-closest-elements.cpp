f811c3aac0a9352b2974706f2efe6fb97f1faaa2
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-03-31 19:40:42 -0400

    LC-find-k-closest-elements [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/find-k-closest-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        /**
+        k = 4 and x = 3
+        |1 - 3| = 2
+        |2 - 3| = 1
+        |3 - 3| = 0
+        |4 - 3| = 1
+        |5 - 3| = 2
+
+        basically return the 4 closest int to x
+        x = 3
+
+        as its sorted think of it like this 
+        u can solve this in this way 
+        calculate it 
+        **/
+        
+    }
+};
