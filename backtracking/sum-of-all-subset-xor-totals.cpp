e9d4e29649ec3d76e142fea1902a0c47730b6238
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-04-01 17:30:41 -0400

    LC-sum-of-all-subset-xor-totals [Runtime: 26 ms; Memory: 20.8 MB]

--- backtracking/sum-of-all-subset-xor-totals.cpp
+++ backtracking/sum-of-all-subset-xor-totals.cpp
@@ -1,14 +1,45 @@
 class Solution {
+
+    void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& result){
+        result.push_back(current);
+
+        for(int i=start; i<nums.size(); i++){
+            current.push_back(nums[i]);
+            backtrack(i+1, nums, current, result);
+            current.pop_back();
+        }
+    }
 public:
     int subsetXORSum(vector<int>& nums) {
         
-        int orOfAll = 0;
-        for(int num : nums){
-            orOfAll |= num;
+        vector<vector<int>> result;
+        vector<int> current;
+
+        backtrack(0, nums, current, result);
+
+        int xorOfAllSubsets;
+        int sum = 0;
+        for(auto& list : result){
+            xorOfAllSubsets = 0;
+            for(int n : list){
+                xorOfAllSubsets ^= n;
+            }
+            sum += xorOfAllSubsets;
         }
 
-        int multiplier = 1 << (nums.size()-1);
+        return sum;
+        
 
-        return orOfAll * multiplier;
+
+
+        // int orOfAll = 0;
+        // for(int num : nums){
+        //     orOfAll |= num;
+        // }
+
+        // //2^(n-1)
+        // int multiplier = 1 << (nums.size()-1);
+
+        // return orOfAll * multiplier;
     }
 };
