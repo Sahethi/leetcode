b3ecc51b6b5a20c266d39839578a039ffa916d75
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-04-04 02:18:28 -0400

    LC-lemonade-change [Runtime: N/A; Memory: N/A]

---
+++ array/lemonade-change.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int countOf5 = 0;
+
+        for(int i=0; i<bills.size(); i++){
+            if(bills[i] == 5){
+                countOf5++;
+            }else{
+                int n = (bills[i] / 5) - 1;
+                if(n <= countOf5) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
