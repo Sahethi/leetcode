1b53631e03771c37759fed4d62b3f26e62709e15
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-04-09 23:25:59 -0400

    LC-search-in-rotated-sorted-array-ii [Runtime: 0 ms; Memory: 18 MB]

--- array/search-in-rotated-sorted-array-ii.cpp
+++ array/search-in-rotated-sorted-array-ii.cpp
@@ -12,14 +12,14 @@ public:
             if(nums[left] == nums[mid] && nums[mid] == nums[right]){
                 left++;
                 right--;
-            } else if(nums[left] < nums[mid]){
+            } else if(nums[left] <= nums[mid]){
                 //left half is sorted so?
                 if(nums[left] <= target && target < nums[mid]){
                     right = mid-1;
                 }else{
                     left = mid+1;
                 }
-            }else if(nums[mid] < nums[right]){
+            }else{
                 //right half is sorted
                 if(nums[mid] < target && target <= nums[right]){
                     left = mid+1;
