49a575fa812b135d36230947591fd47995824f7a
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-04-11 21:04:36 -0400

    LC-closest-binary-search-tree-value [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/closest-binary-search-tree-value.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        int ans = root->val;
+
+        while(root != NULL){
+
+            if(abs(root->val - target) < abs(ans - target)){
+                ans = root->val;
+            }else{
+                if(target < root->val){
+                    //move left
+                    root = root->left;
+                }else{
+                    root = root->right;
+                }
+            }
+        }
+
+        return ans;
+
+    }
+};
