766e0048904a34ce847663c80a00f330785e3585
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-04-13 16:07:23 -0400

    LC-closest-binary-search-tree-value-ii [Runtime: 3 ms; Memory: 22.4 MB]

---
+++ heap-priority-queue/closest-binary-search-tree-value-ii.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& arr){
+        if(!root) return;
+
+        inorder(root->left, arr);
+        arr.push_back(root->val);
+        inorder(root->right, arr);
+    }
+    vector<int> closestKValues(TreeNode* root, double target, int k) {
+        //this is like inorder traversal. + sliding window or binary search
+        vector<int> arr;
+        inorder(root, arr);
+        int left = 0;
+
+        while(left + k < arr.size()){
+            if(abs(arr[left] - target) > abs(arr[left + k] - target)){
+                left++;
+            }else break;
+        }
+
+        vector<int> result;
+        for(int i=left; i<left+k; i++){
+            result.push_back(arr[i]);
+        }
+
+        return result;
+    }
+};
