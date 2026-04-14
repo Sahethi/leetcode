9bca3f59698b3fc9307b2a9bcd2c6cef05237bf9
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-04-14 16:46:28 -0400

    LC-n-th-tribonacci-number [Runtime: N/A; Memory: N/A]

--- math/n-th-tribonacci-number.cpp
+++ math/n-th-tribonacci-number.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        
-        if(n == 1) return 0;
-        if(n == 2) return 1;
-        if(n == 3) return 2;
+
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        else if(n == 2) return 1;
+        else if(n == 3) return 2;
 
         int first = 0, second = 1, third = 1;
-        int sum = first + second + third;
+        int sum = 0;
 
-        for(int i=third; i<n; i++){
-            sum += i;
+        for(int i=0; i<n; i++){
+            sum = first + second + third;
+            first = second;
+            second = third;
+            third = sum;
         }
 
-        return sum;
+        return first;
         
     }
 };
