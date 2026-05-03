a3ad322741b8998a898c0e4bb5d42b5172985fc2
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-03 01:34:34 -0400

    LC-delete-nodes-and-return-forest [Runtime: 17 ms; Memory: 27.3 MB]

---
+++ binary-tree/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_set<int> toDelete;
+    vector<TreeNode*> forest;  
+
+    TreeNode* dfs(TreeNode* root, bool isRoot){
+        if(!root) return NULL;
+
+        //check if it needs to be deleted
+        bool deleted = toDelete.count(root->val);
+
+        if(isRoot && !deleted){
+            forest.push_back(root);
+        }
+
+        root->left = dfs(root->left, deleted);
+        root->right = dfs(root->right, deleted);
+
+        return deleted ? NULL : root;
+
+    }
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        
+        for(int i : to_delete){
+            toDelete.insert(i);
+        }
+
+        dfs(root, true);
+        return forest;
+    }
+};
