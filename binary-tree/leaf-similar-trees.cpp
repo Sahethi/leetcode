d93aa0b6ca1530bd005975556c84dd2d47dad57a
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-04 01:20:33 -0400

    LC-leaf-similar-trees [Runtime: 0 ms; Memory: 15.2 MB]

---
+++ binary-tree/leaf-similar-trees.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void dfs(TreeNode* root, vector<int>& leaves){
+        if(!root) return;
+
+        if(root->right == NULL && root->left == NULL) {
+            leaves.push_back(root->val);
+            return;
+        }
+
+        dfs(root->left, leaves);
+        dfs(root->right, leaves);
+
+    }
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves1, leaves2;
+
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+
+        if(leaves1 == leaves2) return true;
+
+        return false;
+    }
+};
