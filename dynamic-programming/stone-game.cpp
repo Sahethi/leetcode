059450ae249850d93f0d487df9f60ef8adb39e57
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-07 17:33:00 -0400

    LC-stone-game [Runtime: 0 ms; Memory: 10.4 MB]

--- dynamic-programming/stone-game.cpp
+++ dynamic-programming/stone-game.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        
+        //im so fucking smart, this is first thing i thought as alice starts 
+        //she will always pick the optimal solution so she will alays win
         return true;
+
     }
 };
