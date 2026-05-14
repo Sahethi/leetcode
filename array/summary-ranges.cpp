1e66478dbd735e5545764da64fc0b37bfab42d85
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-14 02:02:16 -0400

    LC-summary-ranges [Runtime: N/A; Memory: N/A]

--- array/summary-ranges.cpp
+++ array/summary-ranges.cpp
@@ -3,22 +3,23 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
     
         vector<string> result;
-
-        for(int i=0; i<nums.size();){
-            int start = nums[i];
+        int i=0;
+        while(i<nums.size()){
+            int start = i;
             int j = i;
-            
-            while(j + 1 < nums.size() && nums[j+1] == nums[j] + 1){
+
+            while(j < nums.size() && nums[j+1] == nums[j]+1){
                 j++;
             }
 
-            if(nums[j] == start){
-                result.push_back(to_string(start));
+            if(nums[j] == nums[start]){
+                result.push_back(to_string(nums[start]));
             }else{
-                result.push_back(to_string(start) + "->" + to_string(nums[j]));
+                result.push_back(to_string(nums[start]) + "->" + to_string(nums[j]));
             }
 
             i = j+1;
+
         }
 
         return result;
