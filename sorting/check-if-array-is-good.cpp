92a55f4897e3789c35d042ac2910c56920693bc6
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-15 01:51:23 -0400

    LC-check-if-array-is-good [Runtime: N/A; Memory: N/A]

--- sorting/check-if-array-is-good.cpp
+++ sorting/check-if-array-is-good.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int n = *max_element(nums.begin(), nums.end());
 
-        if(nums.size() != n+1){
-            return false;
+        sort(nums.begin(), nums.end());
+
+        int n = nums.size();
+
+        for(int i=0; i<n-1; i++){
+            if(nums[i] != i+1) return false;
         }
 
-        vector<int> freq(n+1, 0);
+        if(nums[n-1] != n-1) return false;
 
-        for(int i : nums){
-            freq[i]++;
-        }
-
-        if(freq[n] != 2) return false;
-
-        for(int i=1; i<n; i++){
-            if(freq[i] != 1) return false;
-        }
-        
         return true;
+        
+        
+        // int n = *max_element(nums.begin(), nums.end());
+
+        // if(nums.size() != n+1){
+        //     return false;
+        // }
+
+        // vector<int> freq(n+1, 0);
+
+        // for(int i : nums){
+        //     freq[i]++;
+        // }
+
+        // if(freq[n] != 2) return false;
+
+        // for(int i=1; i<n; i++){
+        //     if(freq[i] != 1) return false;
+        // }
+        
+        // return true;
 
     }
 };
