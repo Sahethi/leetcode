ec07731abfb988ca13eb2aec9f2b5084c257be10
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-18 15:57:48 -0400

    LC-average-of-levels-in-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            vector<int> level;
+            int size = q.size();
+
+            for(int i=0; i<size; i++){
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            int sum = 0;
+            double avg = 0.0;
+            for(int i=0; i<level.size(); i++){
+                sum += level[i];
+                avg = sum / (double)level.size();
+            }
+            result.push_back(avg);
+        }
+        return result;
+    }
+};
