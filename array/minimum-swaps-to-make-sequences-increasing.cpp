e87fc8817671e03c984c61c38e1acac38ace959c
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-05-19 19:53:54 -0400

    LC-minimum-swaps-to-make-sequences-increasing [Runtime: N/A; Memory: N/A]

---
+++ array/minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        
+        int n = nums1.size();
+        int keep = 0, swap = 1;
+
+        for(int i=1; i<n; i++){
+            int newKeep = INT_MAX;
+            int newSwap = INT_MAX;
+
+            if(nums1[i-1] < nums1[i] && nums2[i-1] < nums2[i]){
+                newKeep = min(newKeep, keep);
+                newSwap = min(newSwap, swap+1);
+            }else if(nums1[i-1] < nums2[i] && nums2[i-1] < nums1[i]){
+                newKeep = min(newKeep, swap);
+                newSwap = min(newSwap, keep+1);
+            }
+
+            keep = newKeep;
+            swap = newSwap;
+
+
+        }
+
+        return min(keep, swap);
+    }
+};
