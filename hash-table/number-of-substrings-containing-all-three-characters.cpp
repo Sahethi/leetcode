26d1fa9d90eda24e39bfd452c3410f5bf8b84bf5
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-06-30 15:44:13 -0700

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

--- hash-table/number-of-substrings-containing-all-three-characters.cpp
+++ hash-table/number-of-substrings-containing-all-three-characters.cpp
@@ -4,15 +4,15 @@ public:
         vector<int> lastSeen(3, -1);
 
         int count = 0;  
-        int n = s.length();
+        int n = s.size();
 
         for(int i=0; i<n; i++){
             
             lastSeen[s[i] - 'a'] = i;
 
             if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
-                int minIdx = min({lastSeen[0], lastSeen[1], lastSeen[2]});
-                count += (minIdx + 1);
+                count += min({lastSeen[0], lastSeen[1], lastSeen[2]}) + 1;
+                // count += (minIdx + 1);
             }
         }
 
