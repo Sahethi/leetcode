f3ba16b6d94c9e37471be5d3f3a55fa5c789427f
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-06-30 15:43:20 -0700

    LC-number-of-substrings-containing-all-three-characters [Runtime: N/A; Memory: N/A]

--- hash-table/number-of-substrings-containing-all-three-characters.cpp
+++ hash-table/number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        std::vector<int> last_seen(3, -1);
-        int total_substrings = 0;
+        vector<int> lastSeen(3, -1);
+
+        int count = 0;  
         int n = s.length();
-        
-        for (int i = 0; i < n; ++i) {
-            // Update the map for the current character
-            last_seen[s[i] - 'a'] = i;
+
+        for(int i=0; i<n; i++){
             
-            // If we have seen all three characters at least once
-            if (last_seen[0] != -1 && last_seen[1] != -1 && last_seen[2] != -1) {
-                // The window must at least start at the minimum of the three last seen indices
-                int min_idx = std::min({last_seen[0], last_seen[1], last_seen[2]});
-                
-                // All substrings starting from index 0 up to min_idx and ending at i are valid
-                total_substrings += (min_idx + 1);
+            lastSeen[s[i] - 'a'] = i;
+
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+                int minIdx = min({lastSeen[0], lastSeen[1], lastSeen[2]});
+                count += (minIdx + 1);
             }
         }
-        
-        return total_substrings;
+
+        return count;
     }
 };
