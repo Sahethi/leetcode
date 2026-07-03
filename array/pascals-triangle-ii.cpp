f406051a876841fac1eab531e2c49cdf54514e1f
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-03 13:32:15 -0700

    LC-pascals-triangle-ii [Runtime: N/A; Memory: N/A]

---
+++ array/pascals-triangle-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex == 0) return {1};
+        if(rowIndex == 1) return {1,1};
+        
+        /**
+        1 = 0
+        11 = 1
+        121 = 2 = 11 * 11
+        1331 = 3 = 121 * 11
+        14641 = 4 = 1331 * 11
+        **/
+        
+        int n = rowIndex;
+        vector<int> res = {1};
+        int ans = 1;
+        for(int i=1; i<rowIndex; i++){
+            ans = ans * (n / i);
+            n--;
+            res.push_back(ans);
+        }
+        res.push_back(1);
+        return res;
+    }
+};
