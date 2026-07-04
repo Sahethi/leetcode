d8fadcb4a43e5130696a30e4e5b437e2d8f707ad
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-04 13:42:46 -0700

    LC-shortest-word-distance [Runtime: N/A; Memory: N/A]

--- array/shortest-word-distance.cpp
+++ array/shortest-word-distance.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
         
-        int word1Index, word2Index;
+        int word1Index = -1, word2Index = -1;
         int n = wordsDict.size();
-
+        int minDist = INT_MAX;
         for(int i=0; i<n; i++){
             if(wordsDict[i] == word1){
                 word1Index = i;
@@ -12,8 +12,12 @@ public:
             if(wordsDict[i] == word2){
                 word2Index = i;
             }
+
+            if(word2Index != -1 && word1Index != -1){
+                minDist = min(minDist, abs(word1Index - word2Index));
+            }
         }
 
-        return abs(word1Index - word2Index);
+        return minDist;
     }
 };
