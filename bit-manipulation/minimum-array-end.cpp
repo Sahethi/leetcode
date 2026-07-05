ed17b0e423f3ada6d05aceffee92c6eb9e1c2161
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-04 23:26:52 -0700

    LC-minimum-array-end [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/minimum-array-end.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        vector<long long> nums;
+
+        nums.push_back(x);
+        long long candidate = x + 1ll;
+
+        while(nums.size() < n){
+            if((candidate & x) == x) {
+                nums.push_back(candidate);
+            }
+            candidate++;
+        }
+
+        return nums[n-1];
+    }
+};
