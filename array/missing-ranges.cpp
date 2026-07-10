24b95164c2901f81317927f0322a935c04ea4db5
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-09 20:14:21 -0700

    LC-missing-ranges [Runtime: N/A; Memory: N/A]

---
+++ array/missing-ranges.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        if(lower == upper) return {};
+        vector<vector<int>> res;
+        int prev = lower - 1;
+        for(int i=0; i<nums.size(); i++){
+            if(prev + 1 > nums[i] - 1){
+                prev = nums[i];
+                continue;
+            }else{
+                res.push_back({prev + 1 , nums[i] - 1}); 
+                prev = nums[i];
+            }
+        }
+
+        res.push_back({prev + 1 , upper});
+
+        return res;
+    }
+};
