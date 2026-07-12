652fa7197bd4c7f8aa001e8741510d4edda7c877
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-12 16:05:23 -0700

    LC-longest-palindrome [Runtime: N/A; Memory: N/A]

--- hash-table/longest-palindrome.cpp
+++ hash-table/longest-palindrome.cpp
@@ -2,27 +2,54 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         
-        unordered_map<char, int> freq;
+        int freq[52] = {0};
 
         for(char c : s){
-            freq[c]++;
+            int index;
+            if(c >= 'A' && c <= 'Z'){
+                index = c - 'A';
+            }else{
+                index = c - 'a' + 26; 
+            }
+            freq[index]++;
         }
-
+        
         int length = 0;
-        int count = 0;
-        for(auto [c, inc] : freq){
-            if(inc%2 ==1){
-                count++;
-            }
-            if(inc >= 2){
-                length += 2 * (inc/2);
+        bool odd = false;
+
+        for(int count : freq){
+
+            length += (count/2) * 2;
+
+            if(count % 2 == 1){
+                odd = true;
             }
         }
 
-        if(count > 0){
-            length += 1;
-        }
+        return odd ? length + 1 : length;
 
-        return length;
+
+        // unordered_map<char, int> freq;
+
+        // for(char c : s){
+        //     freq[c]++;
+        // }
+
+        // int length = 0;
+        // int count = 0;
+        // for(auto [c, inc] : freq){
+        //     if(inc%2 ==1){
+        //         count++;
+        //     }
+        //     if(inc >= 2){
+        //         length += 2 * (inc/2);
+        //     }
+        // }
+
+        // if(count > 0){
+        //     length += 1;
+        // }
+
+        // return length;
     }
 };
