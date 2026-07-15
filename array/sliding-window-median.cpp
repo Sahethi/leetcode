ac83230dfb54f0d11d89d6e1e575bde536e3dc83
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-14 20:18:36 -0700

    LC-sliding-window-median [Runtime: N/A; Memory: N/A]

--- array/sliding-window-median.cpp
+++ array/sliding-window-median.cpp
@@ -3,6 +3,35 @@ public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
         
         vector<double> res;
+        multiset<int> window;
+        int n = nums.size();
+
+        for(int i=0; i<k; i++){
+            window.insert(nums[i]);
+        }
+
+        for(int i=0; i<=n-k; i++){
+            auto it = window.begin();
+
+            advance(it, k/2);
+            
+            // double m;
+            if(k % 2 == 1){
+                res.push_back(*it);
+            }else{
+                auto prevIt = prev(it);
+                res.push_back(((double)(*prevIt + *it)) / 2.0);
+            }
+
+            if(i == n-k) break;
+
+            window.erase(window.find(nums[i]));
+            window.insert(nums[i+k]);
+        }
+
+        return res;
+        /**
+        vector<double> res;
         int n = nums.size();
 
         for(int i=0; i<=n-k; i++){
@@ -22,5 +51,6 @@ public:
         }      
 
         return res;
+        */
     }
 };
