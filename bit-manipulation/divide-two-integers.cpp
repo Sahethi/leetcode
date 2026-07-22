ad341e3957cd0348e8a2a58f22a3d0ad32d63083
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-21 22:56:07 -0700

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

--- bit-manipulation/divide-two-integers.cpp
+++ bit-manipulation/divide-two-integers.cpp
@@ -2,9 +2,17 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
 
+        /**
+        we check this 
+        INT_MIN = -2147483648
+        and if divisor is -1
+        and this can result in 2147483648
+        */
+
         if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
 
+        //same hai to 0 diff hai toh 1
         bool negative = (dividend < 0) ^ (divisor < 0);
 
         long long dvd = llabs((long long)dividend);
@@ -12,12 +20,11 @@ public:
 
         long long ans = 0;
 
-        while (dvd >= dvs) {
-
+        while(dvd >= dvs){
             long long temp = dvs;
             long long multiple = 1;
 
-            while ((temp << 1) <= dvd) {
+            while((temp << 1) <= dvd){
                 temp <<= 1;
                 multiple <<= 1;
             }
@@ -26,9 +33,29 @@ public:
             ans += multiple;
         }
 
-        if (negative)
+        if(negative)
             ans = -ans;
+        
 
         return (int)ans;
+
+        // while (dvd >= dvs) {
+
+        //     long long temp = dvs;
+        //     long long multiple = 1;
+
+        //     while ((temp << 1) <= dvd) {
+        //         temp <<= 1;
+        //         multiple <<= 1;
+        //     }
+
+        //     dvd -= temp;
+        //     ans += multiple;
+        // }
+
+        // if (negative)
+        //     ans = -ans;
+
+        // return (int)ans;
     }
 };
