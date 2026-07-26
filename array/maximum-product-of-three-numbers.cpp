cce97a6b7510174076a624f8e75752618e6d053a
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-07-26 12:41:09 -0700

    LC-maximum-product-of-three-numbers [Runtime: N/A; Memory: N/A]

--- array/maximum-product-of-three-numbers.cpp
+++ array/maximum-product-of-three-numbers.cpp
@@ -5,7 +5,12 @@ public:
         sort(nums.begin(), nums.end());
         
         int n = nums.size();
-        
-        return nums[n-1] * nums[n-2] * nums[n-3];
+        int mul = 0;
+        if(nums[0] < 0 && nums[1] < 0){
+            mul = nums[0] * nums[1];
+        }
+        int negProd = mul * nums[n-1];
+
+        return max(negProd, nums[n-1] * nums[n-2] * nums[n-3]);
     }
 };
