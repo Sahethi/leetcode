d4c5cde2a49c97fea5f20b3c022b186966a78238
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-08-02 13:39:01 -0700

    LC-max-consecutive-ones-iii [Runtime: 0 ms; Memory: 69.5 MB]

--- sliding-window/max-consecutive-ones-iii.cpp
+++ sliding-window/max-consecutive-ones-iii.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         
-        int left = 0, right = 0, count = 0, maxLen = 0;
-        for(int right=0; right<nums.size(); right++){
+        int left = 0, maxLen = 0, count=0;
+
+        for(int right = 0; right<nums.size(); right++){
             if(nums[right] == 0){
                 count++;
             }
+
             while(count > k){
-                if(nums[left] == 0){
+                if(nums[left] == 0)
                     count--;
-                }
                 left++;
             }
 
