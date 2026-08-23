3df9779f49c8d02f3c9629a4e80c0724905c137e
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-08-23 09:06:52 -0700

    LC-distribute-candies [Runtime: N/A; Memory: N/A]

--- array/distribute-candies.cpp
+++ array/distribute-candies.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         //n is always even 
-        unordered_set<int> uniqueEl;
+        unordered_set<int> uniqueEl(candyType.begin(), candyType.end());
         int n = candyType.size();
-        for(int c : candyType){
-            uniqueEl.insert(c);
-        }
-
         int ans = n/2;
         int setSize = uniqueEl.size();
         ans = min(ans, setSize);
