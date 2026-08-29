568d2181bd624714f573d6097efee7e9fcb95fba
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-08-29 10:39:35 -0700

    LC-binary-tree-tilt [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/binary-tree-tilt.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int totalTilt = 0;
+    int dfs(TreeNode* node){
+        if(node == NULL) return 0;
+
+        int leftSum = 0, rightSum = 0;
+        leftSum = dfs(node->left);
+        rightSum = dfs(node->right);
+
+        totalTilt += abs(leftSum - rightSum);
+        return node->val + leftSum + rightSum;
+    }
+public:
+    int findTilt(TreeNode* root) {
+        //we need to return the sum of all the tilts
+        //tilt = |sum of all nodes in right subtree - sum of all nodes in left subtree|
+        dfs(root);
+        return totalTilt;
+    }
+};
