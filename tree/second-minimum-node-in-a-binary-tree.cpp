72b8e0fabc87418c9027fd419e6aad0b9d382c7d
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-09-02 05:19:21 -0700

    LC-second-minimum-node-in-a-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/second-minimum-node-in-a-binary-tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        if(root == NULL) return -1;
+        if(root->left->val == root->right->val) return -1;
+
+        if(root->right == NULL && root->left != NULL) return root->left->val;
+
+        return root->right->val;
+    }
+};
