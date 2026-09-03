751b84cb0d20b1a4de4152487fb5357d49efba6e
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-09-03 08:25:34 -0700

    LC-binary-tree-maximum-path-sum [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+    int dfs(TreeNode* root, int& maxSum){
+        if(!root) return 0;
+
+        //to remove negative 
+        int leftSum = max(dfs(root->left, maxSum), 0);
+        int rightSum = max(dfs(root->right, maxSum), 0);
+
+        maxSum = max(maxSum, leftSum + rightSum + root->val);
+        
+        return max(leftSum, rightSum) + root->val;
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxSum = root->val;
+        dfs(root, root->val);
+        return maxSum;
+    }
+};
