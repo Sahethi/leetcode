b52f1c75c106e8c710f5c095502d747906749d03
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-09-05 09:28:40 -0700

    LC-contains-duplicate-ii [Runtime: 133 ms; Memory: 134.2 MB]

--- array/contains-duplicate-ii.cpp
+++ array/contains-duplicate-ii.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n = nums.size();
-        // int j = 1;
-        for(int i=0; i<n; i++){
-            int j = i+1;
-            while(j < n && j-i <= k){
-                if(nums[i] == nums[j])
-                    return true;
-                j++;
-            }
-            // j++;
+        // int n = nums.size();
+        // // int j = 1;
+        // for(int i=0; i<n; i++){
+        //     int j = i+1;
+        //     while(j < n && j-i <= k){
+        //         if(nums[i] == nums[j])
+        //             return true;
+        //         j++;
+        //     }
+        //     // j++;
+        // }
+
+        // return false;
+
+        unordered_set<int> window;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (window.count(nums[i]))
+                return true;
+
+            window.insert(nums[i]);
+
+            if (window.size() > k)
+                window.erase(nums[i - k]);
         }
 
         return false;
