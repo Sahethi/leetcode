84d45479d41b2cdf5240e51cd75cca98a4e929d3
Sahethi <40771676+Sahethi@users.noreply.github.com>
2026-09-16 21:06:44 -0400

    LC-construct-uniform-parity-array-i [Runtime: 7 ms; Memory: 30.3 MB]

---
+++ array/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        bool hasEven = false, hasOdd = false;
+
+        // for(int n : nums1){
+        //     if(n % 2 == 0){
+        //         hasEven = true;
+        //     }else{
+        //         hasOdd = true;
+        //     }
+        // }
+
+        // if(hasEven == true && hasOdd == false) return true;
+        // if(hasOdd == true && hasEven == false) return true;
+
+        return true;
+    }
+};
